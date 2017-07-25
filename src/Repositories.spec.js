80acf9b47eabc3c8bd10ce9d01adfa76b1755832
Ian8829 <ghjkghj@naver.com>
2017-07-26 08:25:33 +0900

    use wrapper test components

---
+++ src/Repositories.spec.js
@@ -0,0 +1,46 @@
+// Libs
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+// Components
+import Repositories from './Repositories';
+import Repository from "./Repository";
+
+const repos = [
+  {
+    id: 1,
+    stargazers_count: 2,
+    url: 'www.google.com/1',
+    name: 'best_repo_ever',
+    language: 'javascript'
+  },
+  {
+    id: 2,
+    stargazers_count: 17,
+    url: 'www.google.com/2',
+    name: 'even_better_repo',
+    language: 'C#'
+  },
+  {
+    id: 3,
+    stargazers_count: 4,
+    url: 'www.google.com/3',
+    name: 'ok_repo',
+    language: 'pascal'
+  },
+  {
+    id: 4,
+    stargazers_count: 0,
+    url: 'www.google.com/4',
+    name: 'new_repo',
+    language: 'c++'
+  }
+];
+
+describe('<Repositories />', () => {
+  it('should render number of repos specified by top prop', () => {
+    const wrapper = shallow(<Repositories repositories={repos} top={2} />);
+    expect(wrapper.find(Repository)).to.have.length(2);
+  });
+});
